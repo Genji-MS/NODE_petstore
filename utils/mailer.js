dc1f193d7c6e81e55b5bc6764e59f4c873fb297f
Genji-MS <Genji.Tapia@students.makeschool.com>
2021-04-06 21:31:18 -0700

    Mailer util added, however pets.js incomplete as using stripe account we don't have

---
+++ utils/mailer.js
@@ -0,0 +1,37 @@
+// mailer.js
+
+// require our mailgun dependencies
+const nodemailer = require('nodemailer');
+const mg = require('nodemailer-mailgun-transport');
+// auth with our mailgun API key and domain
+const auth = {
+  auth: {
+    api_key: process.env.MAILGUN_API_KEY,
+    domain: process.env.EMAIL_DOMAIN
+  }
+}
+// create a mailer
+const nodemailerMailgun = nodemailer.createTransport(mg(auth));
+
+// SEND EMAIL
+module.exports.sendMail = (user, req, res) => {
+    // send an email to the user's email with a provided template
+    nodemailerMailgun.sendMail({
+        from: 'no-reply@example.com',
+        to: user.email, // An array if you have multiple recipients.
+        subject: 'Pet Purchased!',
+        template: {
+            name: 'email.handlebars',
+            engine: 'handlebars',
+            context: user
+        }
+    // mail sent, redirect to purchase page
+    }).then(info => {
+        console.log('Response: ' + info);
+        res.redirect(`/pets/${req.params.id}`);
+        // catch errosr and redirect regardless
+    }).catch(err => {
+        console.log('Error: ' + err);
+        res.redirect(`/pets/${req.params.id}`);
+    });
+}
