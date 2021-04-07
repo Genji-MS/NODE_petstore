c59b61e424af897baa4dae327abf17a505692fa1
Genji-MS <Genji.Tapia@students.makeschool.com>
2021-04-06 21:06:54 -0700

    Fixes to connect to db

--- server.js
+++ server.js
@@ -10,6 +10,18 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override')
+// require our mailgun dependencies
+const nodemailer = require('nodemailer');
+const mg = require('nodemailer-mailgun-transport');
+// auth with our mailgun API key and domain
+const auth = {
+  auth: {
+    api_key: process.env.MAILGUN_API_KEY,
+    domain: process.env.EMAIL_DOMAIN
+  }
+}
+// create a mailer
+const nodemailerMailgun = nodemailer.createTransport(mg(auth));
 
 const app = express();
 
@@ -33,6 +45,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 require('./routes/index.js')(app);
 require('./routes/pets.js')(app);
+
+
+// SEND EMAIL
+const user = {
+  email: 'Genji.Tapia@students.makeschool.com',
+  name: 'Genji'
+};
+nodemailerMailgun.sendMail({
+  from: 'no-reply@example.com',
+  to: user.email, // An array if you have multiple recipients.
+  subject: 'Hey you, awesome!',
+  template: {
+    name: 'email.handlebars',
+    engine: 'handlebars',
+    context: user
+  }
+}).then(info => {
+  console.log('Response: ' + info);
+}).catch(err => {
+  console.log('Error: ' + err);
+});
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
